423759931cc5dbd1fdd4afdd4ae13c235d55fecc
Elena Sitnikova <lnstnkv2002@gmail.com>
2023-01-12 22:20:55 +0700

    add test for FindAnimal, TargetMover

---
+++ TestProject1/TestMap.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection;
+using System.Windows.Forms;
+using NUnit.Framework;
+using Moq;
+using WindowsFormsApp1;
+
+namespace TestProject1
+{
+    public class TestMap
+    {
+        private Map _map;
+        private Random random;
+        private Land[,] _land;
+        private StartSimulation _startSimulation;
+
+        [SetUp]
+        public void Setup()
+        {
+            
+            random = new Random();
+            var sizeLand = 1000;
+            _land = new Land[sizeLand, sizeLand];
+            _map = new Map(random);
+            Animal animal;
+
+            for (var i = 0; i < sizeLand; i++)
+            {
+                for (var j = 0; j < sizeLand; j++)
+                {
+                    _land[i, j] = new Land();
+
+                    animal = i switch
+                    {
+                        var index when (i % 10 == 1) => new Eagle(1, 2, _map, random, _land),
+                        var index when (i % 10 == 2) => new Pig(2, 250, _map, random, _land),
+                        var index when (i % 10 == 3) => new Horse(10, 10, _map, random, _land),
+                        var index when (i % 10 == 4) => new Squirrel(100, 100, _map, random, _land),
+                        var index when (i % 10 == 5) => new Mouse(50, 50, _map, random, _land),
+                        var index when (i % 10 == 6) => new Lynx(250, 250, _map, random, _land),
+                        var index when (i % 10 == 7) => new Owl(25, 35, _map, random, _land),
+                        var index when (i % 10 == 8) => new Rabbit(80, 100, _map, random, _land),
+                        var index when (i % 10 == 9) => new Elephant(500, 70, _map, random, _land),
+                        _ => new Eagle(500, 500, _map, random, _land)
+                    };
+                    _land[i, j].SetAnimal(animal);
+                }
+            }
+
+            Type typeMap = typeof(Map);
+            FieldInfo landInfo = typeMap.GetField("_land", BindingFlags.Instance | BindingFlags.NonPublic);
+            landInfo.SetValue(_map, _land);
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimal_NearEagle_ReturnEagle()
+        {
+            var expected = typeof(Eagle);
+
+            var actual = _map.FindAnimal(new Point(1, 1));
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimal_NearOwl_ReturnOwl()
+        {
+            var expected = typeof(Owl);
+
+            var actual = _map.FindAnimal(new Point(8, 2));
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimal_NearRabbit_ReturnRabbit()
+        {
+            var expected = typeof(Rabbit);
+
+            var actual = _map.FindAnimal(new Point(9, 3));
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimal_NearSquirrel_ReturnSquirrel()
+        {
+            var expected = typeof(Squirrel);
+
+            var actual = _map.FindAnimal(new Point(25, 25));
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimal_NearElephant_ReturnElephant()
+        {
+            var expected = typeof(Elephant);
+
+            var actual = _map.FindAnimal(new Point(100, 100));
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimal_NearLynx_ReturnLynx()
+        {
+            var expected = typeof(Lynx);
+
+            var actual = _map.FindAnimal(new Point(967, 215));
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimal_NearHorse_ReturnHorse()
+        {
+            var expected = typeof(Horse);
+
+            var actual = _map.FindAnimal(new Point(4, 250));
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimal_NearMouse_ReturnMouse()
+        {
+            var expected = typeof(Mouse);
+
+            var actual = _map.FindAnimal(new Point(96, 250));
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimal_NearPig_ReturnPig()
+        {
+            var expected = typeof(Pig);
+
+            var actual = _map.FindAnimal(new Point(93, 250));
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimalCouple_ForHorse_ReturnHorse()
+        {
+            var horse = new Horse(1, 1, _map, random, _land);
+            var expected = typeof(Horse);
+
+            var actual = _map.FindCouple(horse);
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+        
+        // Класс хороших данных
+        [Test]
+        public void FindAnimalCouple_ForMouse_ReturnMouse()
+        {
+            var animal = new Mouse(1, 1, _map, random, _land);
+            var expected = typeof(Mouse);
+
+            var actual = _map.FindCouple(animal);
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimalCouple_ForPig_ReturnPig()
+        {
+            var pig = new Pig(1, 1, _map, random, _land);
+            var expected = typeof(Pig);
+
+            var actual = _map.FindCouple(pig);
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimalCouple_ForSquirrel_ReturnSquirrel()
+        {
+            var squirrel = new Squirrel(1, 1, _map, random, _land);
+            var expected = typeof(Squirrel);
+
+            var actual = _map.FindCouple(squirrel);
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimalCouple_ForLynx_ReturnLynx()
+        {
+            var lynx = new Lynx(1, 1, _map, random, _land);
+            var expected = typeof(Lynx);
+
+            var actual = _map.FindCouple(lynx);
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimalCouple_ForOwl_ReturnOwl()
+        {
+            var owl = new Owl(1, 1, _map, random, _land);
+            var expected = typeof(Owl);
+
+            var actual = _map.FindCouple(owl);
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimalCouple_ForRabbit_ReturnRabbit()
+        {
+            var rabbit = new Rabbit(1, 1, _map, random, _land);
+            var expected = typeof(Rabbit);
+
+            var actual = _map.FindCouple(rabbit);
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimalCouple_ForEagle_ReturnEagle()
+        {
+            var eagle = new Eagle(1, 1, _map, random, _land);
+            var expected = typeof(Eagle);
+
+            var actual = _map.FindCouple(eagle);
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimalCouple_ForElephant_ReturnElephant()
+        {
+            var elephant = new Elephant(1, 1, _map, random, _land);
+            var expected = typeof(Elephant);
+
+            var actual = _map.FindCouple(elephant);
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimalCouple_ForFemale_ReturnMale()
+        {
+            var female = new Female(1, 1, _map, random, _land);
+            var expected = typeof(Male);
+
+            var actual = _map.FindCouple(female);
+
+            Assert.AreEqual(expected, actual.GetType());
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void FindAnimalCouple_ForMale_ReturnFemale()
+        {
+            var male = new Male(1, 1, _map, random, _land);
+            var expected = typeof(Female);
+
+            var actual = _map.FindCouple(male);
+            
+            Assert.AreEqual(expected, actual.GetType());
+        }
+     
+        
+    }
+}
---
+++ TestProject1/TestTargetMover.cs
@@ -0,0 +1,280 @@
+using System;
+using System.Drawing;
+using NUnit.Framework;
+using Moq;
+using WindowsFormsApp1;
+
+
+namespace TestProject1
+{
+    public class TestTargetMover
+    {
+        // Класс хороших данных 
+        [Test]
+        public void MoveTargetEuclid_DownToPositiveDirection_ReturnPoint()
+        {
+            var targetMover = new TargetMoverEucliden();
+            var expected = new Point(1, 0);
+
+            var actual = targetMover.TargetMove(new Point(1, 1), new Point(1, 0));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void MoveTargetEuclid_UpToPositiveDirection_ReturnPoint()
+        {
+            var targetMover = new TargetMoverEucliden();
+            var expected = new Point(5, 2);
+
+            var actual = targetMover.TargetMove(new Point(5, 1), new Point(5, 2));
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        // Класс хороших данных
+        [Test]
+        public void MoveTargetEuclid_OnePlace_ReturnPoint()
+        {
+            var targetMover = new TargetMoverEucliden();
+            var expected = new Point(5, 5);
+
+            var actual = targetMover.TargetMove(new Point(5, 5), new Point(5, 5));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        // Класс хороших данных
+        [Test]
+        public void MoveTargetEuclid_ToLeftSidePositiveDirection_ReturnPoint()
+        {
+            var targetMover = new TargetMoverEucliden();
+            var expected = new Point(1, 1);
+
+            var actual = targetMover.TargetMove(new Point(0, 1), new Point(1, 1));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс плохих данных
+        [Test]
+        public void MoveTargetEuclid_OffMapToLeft_ReturnOriginCoordinate()
+        {
+            var targetMover = new TargetMoverEucliden();
+            var expected = new Point(5, 4);
+
+            var actual = targetMover.TargetMove(new Point(6, 4), new Point(5, 4));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс плохих данных
+        [Test]
+        public void MoveTargetEuclid_OffMapToLeftAndSameY_ReturnOriginCoordinate()
+        {
+            var targetMover = new TargetMoverEucliden();
+            var expected = new Point(0, 0);
+
+            var actual = targetMover.TargetMove(new Point(0, 0), new Point(-1, 0));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс плохих данных
+        [Test]
+        public void MoveTargetEuclid_OffMapToLeftAndSameX_ReturnOriginCoordinate()
+        {
+            var targetMover = new TargetMoverEucliden();
+            var expected = new Point(0, 0);
+
+            var actual = targetMover.TargetMove(new Point(0, 0), new Point(0, -1));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        // Класс плохих данных
+        [Test]
+        public void MoveTargetEuclid_OffMapToRight_ReturnOriginCoordinate()
+        {
+            var targetMover = new TargetMoverEucliden();
+            var expected = new Point(1000, 1000);
+
+            var actual = targetMover.TargetMove(new Point(1000, 1000), new Point(1002, 10002));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс плохих данных
+        [Test]
+        public void MoveTargetEuclid_OffMapToRightAndSameY_ReturnOriginCoordinate()
+        {
+            var targetMover = new TargetMoverEucliden();
+            var expected = new Point(1000, 1000);
+
+            var actual = targetMover.TargetMove(new Point(1000, 1000), new Point(1002, 1000));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс плохих данных
+        [Test]
+        public void MoveTargetEuclid_OffMapToRightAndSameX_ReturnOriginCoordinate()
+        {
+            var targetMover = new TargetMoverEucliden();
+            var expected = new Point(1000, 1000);
+
+            var actual = targetMover.TargetMove(new Point(1000, 1000), new Point(1000, 10002));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void MoveTargetEuclid_ToRightSidePositiveDirection_ReturnPoint()
+        {
+            var targetMover = new TargetMoverEucliden();
+            var expected = new Point(2, 1);
+
+            var actual = targetMover.TargetMove(new Point(1, 1), new Point(2, 1));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void MoveTargetSavingDirection_ToLeftSidePositiveDirection_ReturnPoint()
+        {
+            var targetMover = new TargetMoverSavingDirection();
+            var expected = new Point(1, 1);
+
+            var actual = targetMover.TargetMove(new Point(0, 1), new Point(1, 1));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void MoveTargetSavingDirection_InPlaceToPositiveDirection_ReturnPoint()
+        {
+            var targetMover = new TargetMoverSavingDirection();
+            var expected = new Point(1, 1);
+
+            var actual = targetMover.TargetMove(new Point(1, 1), new Point(1, 1));
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        // Класс хороших данных
+        [Test]
+        public void MoveTargetSavingDirection_UpToPositiveDirection_ReturnPoint()
+        {
+            var targetMover = new TargetMoverSavingDirection();
+            var expected = new Point(5, 1);
+
+            var actual = targetMover.TargetMove(new Point(5, 0), new Point(5, 1));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс хороших данных
+        [Test]
+        public void MoveTargetSavingDirection_DownToPositiveDirection_ReturnPoint()
+        {
+            var targetMover = new TargetMoverSavingDirection();
+            var expected = new Point(5, 1);
+
+            var actual = targetMover.TargetMove(new Point(5, 2), new Point(5, 1));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+        // Класс хороших данных
+        [Test]
+        public void MoveTargetSavingDirection_TiRightDirection_ReturnPoint()
+        {
+            var targetMover = new TargetMoverSavingDirection();
+            var expected = new Point(5, 4);
+
+            var actual = targetMover.TargetMove(new Point(6, 4), new Point(5, 4));
+
+            Assert.AreEqual(expected, actual);
+        }
+        
+        // Класс плохих данных
+        [Test]
+        public void MoveTargetSavingDirection_WhenOffMapOnLeft_ReturnOriginCoordinate()
+        {
+            var targetMover = new TargetMoverSavingDirection();
+            var expected = new Point(0, 0);
+
+            var actual = targetMover.TargetMove(new Point(0, 0), new Point(-1, -1));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс плохих данных
+        [Test]
+        public void MoveTargetSavingDirection_WhenOffMapOnLeftAndSameY_ReturnOriginCoordinate()
+        {
+            var targetMover = new TargetMoverSavingDirection();
+            var expected = new Point(0, 0);
+
+            var actual = targetMover.TargetMove(new Point(0, 0), new Point(-1, 0));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс плохих данных
+        [Test]
+        public void MoveTargetSavingDirection_WhenOffMapOnLeftAndSameX_ReturnOriginCoordinate()
+        {
+            var targetMover = new TargetMoverSavingDirection();
+            var expected = new Point(0, 0);
+
+            var actual = targetMover.TargetMove(new Point(0, 0), new Point(0, -1));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        // Класс плохих данных
+        [Test]
+        public void MoveTargetSavingDirection_WhenOffMapOnRight_ReturnOriginCoordinate()
+        {
+            var targetMover = new TargetMoverSavingDirection();
+            var expected = new Point(1000, 1000);
+
+            var actual = targetMover.TargetMove(new Point(1000, 1000), new Point(1002, 10002));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс плохих данных
+        [Test]
+        public void MoveTargetSavingDirection_WhenOffMapOnRightAndSameY_ReturnOriginCoordinate()
+        {
+            var targetMover = new TargetMoverSavingDirection();
+            var expected = new Point(1000, 1000);
+
+            var actual = targetMover.TargetMove(new Point(1000, 1000), new Point(1002, 1000));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        // Класс плохих данных
+        [Test]
+        public void MoveTargetSavingDirection_WhenOffMapOnRightAndSameX_ReturnOriginCoordinate()
+        {
+            var targetMover = new TargetMoverSavingDirection();
+            var expected = new Point(1000, 1000);
+
+            var actual = targetMover.TargetMove(new Point(1000, 1000), new Point(1000, 10002));
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
